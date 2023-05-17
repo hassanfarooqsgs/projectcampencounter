afec3660a935331fe63e868ca967cc5fb80d2f83
hassan_farooq <hassanfarooqsgs@gmail.com>
2023-05-17 11:06:37 +0500

    2nddone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :middle_name, :phone_number, :country])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:tos_agreement])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:tos_agreement])
+
+  end
+
+
 end
